d4e443357d833ccd82a809288115ee8e444ba675
Ahmed Farag <faragdeveloper@gmail.com>
2025-02-12 19:01:08 +0200

    Fix gallary images layout

--- components/sections/Gallery1.js
+++ components/sections/Gallery1.js
@@ -10,17 +10,29 @@ const Gallery1 = ({ data }) => {
             <div className="row">
               <div className="col-xl-6">
                 <div className="row">
-                  {gallery.sideImages.map((item, index) => (
-                    <div key={index} className="gallery-block col-sm-6">
+                    <div className="gallery-block col-sm-6">
                       <div className="inner-box">
                         <div className="image-box">
                           <figure className="image">
-                            <img src={item.image} alt={item.alt}/>
+                            <img src={data.gallery.mediumImages[0].image} alt={data.gallery.mediumImages[0].alt}/>
                           </figure>
                         </div>
                       </div>
                     </div>
-                  ))}
+                    <div className="gallery-block col-sm-6">
+                      <div className="inner-box">
+                        <div className="image-box">
+                          <figure className="image">
+                            <img src={data.gallery.smallImages[0].image} alt={data.gallery.smallImages[0].alt}/>
+                          </figure>
+                        </div>
+                        <div className="image-box">
+                          <figure className="image">
+                            <img src={data.gallery.smallImages[1].image} alt={data.gallery.smallImages[1].alt}/>
+                          </figure>
+                        </div>
+                      </div>
+                    </div>
                 </div>
               </div>
               <div className="gallery-block col-xl-6">
